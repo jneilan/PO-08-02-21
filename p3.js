019ac0a353374a4ab5f4d4f22cd643316b504c2e
JN <48257966+jneilan@users.noreply.github.com>
2021-08-02 15:27:16 -0600

    Create p3.js

---
+++ p3.js
@@ -0,0 +1,36 @@
+// Answer:  Q1G3A1B2F3A1G2E4
+
+/* Logical Reasoning:
+This question is based on a certain code language. Understand the logic in the coding and answer the
+following question. 
+If PROCESSOR is coded as D4F3C5C1E1S1S1E3C6, what is the code for QUADRANT? */
+
+/* Logic for plaintext to ciphertext conversion:
+1) Convert each letter to its numerical placement in the alphabet (A=1...Z=26)
+2) Choose any two factors that multiply to that value. If prime, choose that number and 1.
+3) Convert the first factor to its alphabetic equivalent (1=A...26=Z)
+*/
+
+
+/* Example
+Plaintext:      P  R  O  C  E  S   S   O  R
+
+# Equivalence:  16 18 15 3  5  19  19  15 18
+
+Factors:        44 63 35 31 51 191 191 53 36
+
+Ciphertext:     D4 F3 C5 C1 E1 S1  S1  E3 C6
+*/
+
+
+/* Applying to "QUADRANT"
+Plaintext:      Q   U  A  D  R  A  N  T
+
+# Equivalence:  17  21 1  4  18 1  14 20
+
+Factors:        171 73 11 22 63 11 72 54
+
+Ciphertext:     Q1  G3 A1 B2 F3 A1 G2 E4
+*/
+
+// Possible encoding for QUADRANT =>  Q1G3A1B2F3A1G2E4
